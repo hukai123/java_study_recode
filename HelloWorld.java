8f0d97a5764410b08708dc288b76a10870800763
hukai123 <704266819@qq.com>
2018-07-09 19:19:06 +0800

    third

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
+    	// 这是单行注释的示例
+       /* 这个也是单行注释的示例 */
         System.out.println("Hello World");
         System.out.println(args[0]);
     }
